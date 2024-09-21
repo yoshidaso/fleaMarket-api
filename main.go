21f971963cb991be9811b4261fb7e4d97fc4eacb
sota <hitoesouta@gmail.com>
2024-09-21 23:58:24 +0900

    add models

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	//items := []models.Item{
+	//	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", soldOut: false}
+	//}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
