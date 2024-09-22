cbf0bec337eab609008615482407f5e3626828c8
sota <hitoesouta@gmail.com>
2024-09-22 22:23:28 +0900

    add repository

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"gin-freemarket/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//items := []models.Item{
-	//	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", soldOut: false}
-	//}
+	items := []models.Item{
+		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", soldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", soldOut: true},
+		{ID: 1, Name: "商品3", Price: 3000, Description: "説明3", soldOut: false},
+	}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
