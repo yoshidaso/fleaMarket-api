a549073b7e035dd03070efef1036d9f16d339eab
sota <hitoesouta@gmail.com>
2024-10-17 18:44:18 +0900

    add create method and find method

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"gin-freemarket/controllers"
 	"gin-freemarket/infra"
-	"gin-freemarket/models"
+	//"gin-freemarket/models"
 	"gin-freemarket/repositories"
 	"gin-freemarket/services"
 	"github.com/gin-gonic/gin"
@@ -11,13 +11,15 @@ import (
 
 func main() {
 	infra.Initialize()
-	items := []models.Item{
-		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	}
+	db := infra.SetupDB()
+	//items := []models.Item{
+	//	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+	//	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	//	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	//}
 
-	itemRepository := repositories.NewItemMemoryRepository(items)
+	//itemRepository := repositories.NewItemMemoryRepository(items)
+	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemServices(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"errors"
 	"gin-freemarket/models"
+	"gorm.io/gorm"
 )
 
 type IItemRepository interface {
@@ -57,4 +58,51 @@ func (r *ItemMemoryRepository) Delete(itemId uint) error {
 		}
 	}
 	return errors.New("item not found")
+}
+
+type ItemRepository struct {
+	db *gorm.DB
+}
+
+func (r ItemRepository) FindAll() (*[]models.Item, error) {
+	var items []models.Item
+	result := r.db.Find(&items)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &items, nil
+}
+
+func (r ItemRepository) FindById(itemId uint) (*models.Item, error) {
+	var item models.Item
+	result := r.db.First(&item, itemId)
+	if result.Error != nil {
+		if result.Error.Error() == "record not found" {
+			return nil, errors.New("item not found")
+		}
+		return nil, result.Error
+	}
+	return &item, nil
+}
+
+func (r *ItemRepository) Create(newItem models.Item) (*models.Item, error) {
+	result := r.db.Create(&newItem)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &newItem, nil
+}
+
+func (r ItemRepository) Update(updateItem models.Item) (*models.Item, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (i ItemRepository) Delete(itemId uint) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewItemRepository(db *gorm.DB) IItemRepository {
+	return &ItemRepository{db: db}
 }
