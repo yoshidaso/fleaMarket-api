acd7e5798a3ccfad12bab2a892e17ad6cbca506f
sota <hitoesouta@gmail.com>
2024-10-22 15:14:27 +0900

    add router

--- main.go
+++ main.go
@@ -24,11 +24,14 @@ func main() {
 	itemController := controllers.NewItemController(itemService)
 
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
+
+	// ルーティングの設定
+	itemRouter := r.Group("/items")
+	itemRouter.GET("", itemController.FindAll)
 	// idはパスパラメーターなので「:」をつける
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
 	r.Run("localhost:8080")
 }
